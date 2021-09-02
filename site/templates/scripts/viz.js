c74c9ff408e4d23406ce5a142494c55e3530607e
Starlingizer <48607400+Starlingizer@users.noreply.github.com>
2021-09-02 16:11:21 +0100

    Update viz.js

--- site/templates/scripts/viz.js
+++ site/templates/scripts/viz.js
@@ -1,24 +1,4 @@
-/*
-Viz.js 2.1.2 (Graphviz 2.40.1, Expat 2.2.5, Emscripten 1.37.36)
-Copyright (c) 2014-2018 Michael Daines
-Licensed under MIT license
 
-This distribution contains other software in object code form:
-
-Graphviz
-Licensed under Eclipse Public License - v 1.0
-http://www.graphviz.org
-
-Expat
-Copyright (c) 1998, 1999, 2000 Thai Open Source Software Center Ltd and Clark Cooper
-Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006 Expat maintainers.
-Licensed under MIT license
-http://www.libexpat.org
-
-zlib
-Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler
-http://www.zlib.net/zlib_license.html
-*/
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
